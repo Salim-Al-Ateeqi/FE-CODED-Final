6d0c3e1d5469f99251a493e6aabf2346f364696a
fahadalduaij <fahad_osama@hotmail.com>
2021-12-05 16:51:44 +0300

    adding authStore and instance Store

---
+++ stores/instance.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+// Yousef
+// export const baseURL = "http://192.168.8.128:8000";
+
+// Fahad
+export const baseURL = "http://192.168.1.75:8000";
+
+// Faisal
+// export const baseURL = "http://192.168.8.101:8000";
+
+// Salim
+// export const baseURL = "http://192.168.8.101:8000";
+
+export const instance = axios.create({
+	baseURL: `${baseURL}/api`,
+});
