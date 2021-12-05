dca980400a4f8596090eee7381b4b983ff2595ea
CaptSoulBeard <Salim.Al.Ateeqi1@gmail.com>
2021-12-05 16:56:04 +0300

    Added Country Input Library and minor CSS to make it the Sign-up component more viewable

--- Components/Authentication/styles.js
+++ Components/Authentication/styles.js
@@ -1,3 +1,24 @@
-//Sign-up
+import { StyleSheet } from "react-native";
 
-//Sign-in
+const styles = StyleSheet.create({
+  //Main Styling Settings
+
+  background: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "pink",
+  },
+
+  //Sign-up
+
+  signupContainer: {
+    height: "50%",
+    width: "50%",
+    margin: "auto",
+    backgroundColor: "blue",
+  },
+
+  //Sign-in
+});
+
+export default styles;
