ac5b1fb65b13d0c6af4d577b766e1574b2484605
CaptSoulBeard <Salim.Al.Ateeqi1@gmail.com>
2021-12-06 16:53:22 +0300

    Sign-in Done

--- stores/instance.js
+++ stores/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Yousef
-export const baseURL = "http://192.168.20.104:8000";
+// export const baseURL = "http://192.168.20.104:8000";
 
 // Fahad
 // export const baseURL = "http://192.168.1.75:8000";
@@ -10,8 +10,8 @@ export const baseURL = "http://192.168.20.104:8000";
 // export const baseURL = "http://192.168.8.101:8000";
 
 // Salim
-// export const baseURL = "http://192.168.8.109:8000";
+export const baseURL = "http://192.168.8.109:8000";
 
 export const instance = axios.create({
-	baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL}/api`,
 });
