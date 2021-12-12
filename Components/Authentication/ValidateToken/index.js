246d7d8496389064df3c29396c22c4bb1d004fa8
Alkandari-Y <yayalkandari@gmail.com>
2021-12-12 13:56:31 +0300

    custom  drawer still resulting in issues during registration.

--- Components/Authentication/ValidateToken/index.js
+++ Components/Authentication/ValidateToken/index.js
@@ -1,104 +1,115 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
-	Box,
-	Heading,
-	VStack,
-	FormControl,
-	Input,
-	Button,
-	Center,
-	useToast,
-	Spinner,
+  Box,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Button,
+  Center,
+  useToast,
+  Spinner,
 } from "native-base";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { ScrollView } from "react-native-gesture-handler";
 import authStore from "../../../stores/authStore";
 import { observer } from "mobx-react";
 import { Colors } from "../../../assets/Theme/Colors";
+import profileStore from "../../../stores/ProfileStore";
 
 const ValidateTokens = ({ navigation }) => {
-	const toast = useToast();
+  const toast = useToast();
 
-	const [userInput, setUserInput] = useState({ SMSToken: "" });
-	const [matchingToken, setMatchingToken] = useState(false);
+  const [userInput, setUserInput] = useState({ SMSToken: "" });
+  const [matchingToken, setMatchingToken] = useState(false);
 
-	if (!authStore.user) {
-		return <Spinner />;
-	}
+  useEffect(() => {
+    const userProfile = profileStore.profiles.find(
+      (profile) => profile._id === authStore.user._id
+    );
 
-	const userSMSAuthToken = authStore.user.SMSToken;
+    if (!userProfile) {
+      profileStore.getUserProfile();
+    }
+  }, []);
 
-	const checkMatchingToken = () => {
-		userSMSAuthToken !== userInput
-			? setMatchingToken(false)
-			: setMatchingToken(true);
-	};
+  if (!authStore.user) {
+    return <Spinner />;
+  }
 
-	const handleSubmit = () => {
-		checkMatchingToken();
-		authStore.validateToken(userInput, toast, navigation);
-	};
+  const userSMSAuthToken = authStore.user.SMSToken;
 
-	return (
-		<KeyboardAwareScrollView>
-			<ScrollView>
-				<Center mt="20">
-					<Box safeArea p="2" py="8" w="100%" maxW="290">
-						<Heading
-							size="lg"
-							fontWeight="600"
-							color="coolGray.800"
-							_dark={{
-								color: "warmGray.50",
-							}}
-						>
-							SMS Verification
-						</Heading>
-						<Heading
-							mt="1"
-							_dark={{
-								color: "warmGray.200",
-							}}
-							color="coolGray.600"
-							fontWeight="medium"
-							size="xs"
-						>
-							{authStore.message}
-						</Heading>
+  const checkMatchingToken = () => {
+    userSMSAuthToken !== userInput
+      ? setMatchingToken(false)
+      : setMatchingToken(true);
+  };
 
-						{matchingToken && (
-							<Heading mt="3" color="#dc2626" fontWeight="medium" size="xs">
-								Please ensure you've entered the token correctly!
-							</Heading>
-						)}
-						<VStack space={3} mt="5">
-							<FormControl>
-								<FormControl.Label>Please Enter Your Token</FormControl.Label>
-								<Input
-									bg="#fff"
-									h="50"
-									keyboardType={"number-pad"}
-									placeholder="Enter Your Token"
-									_focus={{ borderColor: Colors.primary }}
-									onChangeText={(SMSToken) =>
-										setUserInput({ ...userInput, SMSToken })
-									}
-								/>
-							</FormControl>
+  const handleSubmit = () => {
+    checkMatchingToken();
+    authStore.validateToken(userInput, toast, navigation);
+  };
 
-							<Button
-								mt="2"
-								style={{ backgroundColor: Colors.primary }}
-								onPress={handleSubmit}
-							>
-								Register
-							</Button>
-						</VStack>
-					</Box>
-				</Center>
-			</ScrollView>
-		</KeyboardAwareScrollView>
-	);
+  return (
+    <KeyboardAwareScrollView>
+      <ScrollView>
+        <Center mt="20">
+          <Box safeArea p="2" py="8" w="100%" maxW="290">
+            <Heading
+              size="lg"
+              fontWeight="600"
+              color="coolGray.800"
+              _dark={{
+                color: "warmGray.50",
+              }}
+            >
+              SMS Verification
+            </Heading>
+            <Heading
+              mt="1"
+              _dark={{
+                color: "warmGray.200",
+              }}
+              color="coolGray.600"
+              fontWeight="medium"
+              size="xs"
+            >
+              {authStore.message}
+            </Heading>
+
+            {matchingToken && (
+              <Heading mt="3" color="#dc2626" fontWeight="medium" size="xs">
+                Please ensure you've entered the token correctly!
+              </Heading>
+            )}
+            <VStack space={3} mt="5">
+              <FormControl>
+                <FormControl.Label>Please Enter Your Token</FormControl.Label>
+                <Input
+                  bg="#fff"
+                  h="50"
+                  keyboardType={"number-pad"}
+                  placeholder="Enter Your Token"
+                  _focus={{ borderColor: Colors.primary }}
+                  onChangeText={(SMSToken) =>
+                    setUserInput({ ...userInput, SMSToken })
+                  }
+                />
+              </FormControl>
+
+              <Button
+                mt="2"
+                style={{ backgroundColor: Colors.primary }}
+                onPress={handleSubmit}
+              >
+                Register
+              </Button>
+            </VStack>
+          </Box>
+        </Center>
+      </ScrollView>
+    </KeyboardAwareScrollView>
+  );
 };
 
 export default observer(ValidateTokens);
