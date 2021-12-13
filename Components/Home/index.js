d97299a82e2e80069246c57c99ae4f7718824ca8
fahadalduaij <fahad_osama@hotmail.com>
2021-12-13 18:45:17 +0300

    redesgin create group and broadcast list btn

--- Components/Home/index.js
+++ Components/Home/index.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 import {
-  ScrollView,
-  Spinner,
-  Box,
-  Divider,
-  VStack,
-  HStack,
-  Button,
+	ScrollView,
+	Spinner,
+	Box,
+	Divider,
+	VStack,
+	HStack,
+	Button,
+	Text,
 } from "native-base";
+import { TouchableHighlight, TouchableOpacity } from "react-native";
 
 // components
 import GroupItem from "./GroupItem";
@@ -18,52 +20,62 @@ import Search from "../Search/";
 import groupStore from "../../stores/groupStore";
 import authStore from "../../stores/authStore";
 import { socket } from "../../stores/instance";
+import { Colors } from "../../assets/Theme/Colors";
 
 const Home = ({ navigation }) => {
-  const [query, setQuery] = useState("");
+	const [query, setQuery] = useState("");
 
-  if (groupStore.isLoading) {
-    return <Spinner />;
-  }
+	if (groupStore.isLoading) {
+		return <Spinner />;
+	}
 
-  const user = authStore.user ?? "";
+	const user = authStore.user ?? "";
 
-  const data = authStore.user;
-  socket.emit("authUser", data);
+	const data = authStore.user;
+	socket.emit("authUser", data);
 
-  const groupList = groupStore.groups
-    .filter((_group) => _group.name.toLowerCase().includes(query.toLowerCase()))
-    .filter((__group) => __group.members.includes(user._id))
-    .map((group) => (
-      <GroupItem key={group._id} navigation={navigation} group={group} />
-    ));
+	const groupList = groupStore.groups
+		.filter((_group) => _group.name.toLowerCase().includes(query.toLowerCase()))
+		.filter((__group) => __group.members.includes(user._id))
+		.map((group) => (
+			<GroupItem key={group._id} navigation={navigation} group={group} />
+		));
 
-  const handlePress = () => {
-    navigation.navigate("CreateGroup");
-  };
+	const handlePress = () => {
+		navigation.navigate("CreateGroup");
+	};
 
-  return (
-    <Box bg="white" flex="1">
-      <ScrollView>
-        <Divider />
-        <VStack mt="1">
-          <Search setQuery={setQuery} />
-          <HStack justifyContent="space-between">
-            <Button colorScheme="darkBlue" variant="link">
-              Broadcast List
-            </Button>
+	return (
+		<Box bg="white" flex="1">
+			<ScrollView>
+				<Divider />
+				<VStack mt="1">
+					<Search setQuery={setQuery} />
+					<HStack
+						justifyContent="space-between"
+						alignItems={"center"}
+						mx={4}
+						my={2}
+					>
+						<TouchableOpacity activeOpacity={0.7}>
+							<Text fontWeight={"semibold"} underline color={Colors.primary}>
+								Broadcast List
+							</Text>
+						</TouchableOpacity>
 
-            <Button colorScheme="darkBlue" variant="link" onPress={handlePress}>
-              Create Group
-            </Button>
-          </HStack>
-          <Divider />
-        </VStack>
+						<TouchableOpacity activeOpacity={0.7} onPress={handlePress}>
+							<Text fontWeight={"semibold"} underline color={Colors.primary}>
+								Create Group
+							</Text>
+						</TouchableOpacity>
+					</HStack>
+					<Divider />
+				</VStack>
 
-        {groupList}
-      </ScrollView>
-    </Box>
-  );
+				{groupList}
+			</ScrollView>
+		</Box>
+	);
 };
 
 export default observer(Home);
