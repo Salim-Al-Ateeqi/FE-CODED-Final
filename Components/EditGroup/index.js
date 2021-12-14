c2080ac0de59c4e244219522728a9eb7f9d786d7
Alkandari-Y <yayalkandari@gmail.com>
2021-12-14 16:39:03 +0300

    sockets navig

--- Components/EditGroup/index.js
+++ Components/EditGroup/index.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 import {
-	Text,
-	VStack,
-	ScrollView,
-	Button,
-	Box,
-	Divider,
-	useToast,
-	HStack,
+  Text,
+  VStack,
+  ScrollView,
+  Button,
+  Box,
+  Divider,
+  useToast,
+  HStack,
 } from "native-base";
 
 // components
@@ -22,82 +22,91 @@ import DeleteGroupButton from "./DeleteGroupButton";
 // stores
 import profileStore from "../../stores/ProfileStore";
 import groupStore from "../../stores/groupStore";
+import { socket } from "../../stores/instance";
 
 const EditGroup = ({ route, navigation }) => {
-	const [focusOnInput, setFocusOnInput] = useState(false);
-	const [imageChanged, setImageChanged] = useState(false);
-	const [updatedGroup, setUpdatedGroup] = useState({
-		name: "",
-		image: "",
-	});
-	const toast = useToast();
+  const [focusOnInput, setFocusOnInput] = useState(false);
+  const [imageChanged, setImageChanged] = useState(false);
+  const [updatedGroup, setUpdatedGroup] = useState({
+    name: "",
+    image: "",
+  });
+  const toast = useToast();
 
-	const { group } = route.params;
+  const { group } = route.params;
 
-	const groupMembers = profileStore.profiles
-		.filter((profile) => group.members.includes(profile._id))
-		.map((member) => <MemberItem key={member._id} member={member} />);
+  const groupMembers = profileStore.profiles
+    .filter((profile) => group.members.includes(profile._id))
+    .map((member) => <MemberItem key={member._id} member={member} />);
 
-	const handleUpdate = () => {
-		groupStore.updateGroup(group._id, updatedGroup, navigation, toast);
-	};
+  const handleUpdate = () => {
+    groupStore.updateGroup(group._id, updatedGroup, navigation, toast);
+  };
 
-	console.log(updatedGroup);
+  socket.on("navigate-home", () => {
+    toast.show({
+      title: "Group Has been deleted!",
+      status: "info",
+      placement: "top",
+      duration: 1500,
+    });
+    navigation.navigate("Groups");
+  });
 
-	return (
-		<Box flex="1" w="100%" bg="#fff">
-			<ScrollView>
-				<ImageEditGroup
-					group={group}
-					updatedGroup={updatedGroup}
-					setUpdatedGroup={setUpdatedGroup}
-					imageChanged={imageChanged}
-					setImageChanged={setImageChanged}
-				/>
+  return (
+    <Box flex="1" w="100%" bg="#fff">
+      <ScrollView>
+        <ImageEditGroup
+          group={group}
+          updatedGroup={updatedGroup}
+          setUpdatedGroup={setUpdatedGroup}
+          imageChanged={imageChanged}
+          setImageChanged={setImageChanged}
+        />
 
-				<VStack flex={1} space="5">
-					<InputEditGroup
-						group={group}
-						updatedGroup={updatedGroup}
-						setUpdatedGroup={setUpdatedGroup}
-						imageChanged={imageChanged}
-						focusOnInput={focusOnInput}
-						handleUpdate={handleUpdate}
-					/>
+        <VStack flex={1} space="5">
+          <InputEditGroup
+            group={group}
+            updatedGroup={updatedGroup}
+            setUpdatedGroup={setUpdatedGroup}
+            imageChanged={imageChanged}
+            focusOnInput={focusOnInput}
+            handleUpdate={handleUpdate}
+          />
 
-					<Button
-						alignSelf={"flex-end"}
-						w="25%"
-						mr={3}
-						style={{ backgroundColor: Colors.primary }}
-						_text={{
-							color: Colors.lightBg,
-						}}
-						onPress={handleUpdate}
-					>
-						Save
-					</Button>
+          <Button
+            alignSelf={"flex-end"}
+            w="25%"
+            mr={3}
+            style={{ backgroundColor: Colors.primary }}
+            _text={{
+              color: Colors.lightBg,
+            }}
+            onPress={handleUpdate}
+          >
+            Save
+          </Button>
 
-					<VStack mb={5}>
-						<HStack w="100%">
-							<Text fontSize={12} ml="3" mb="2">
-								{group.members.length} MEMBERS
-							</Text>
-						</HStack>
-						<Divider />
+          <VStack mb={5}>
+            <HStack w="100%">
+              <Text fontSize={12} ml="3" mb="2">
+                {group.members.length} MEMBERS
+              </Text>
+            </HStack>
+            <Divider />
 
-						<AddMembersButton navigation={navigation} group={group} />
-						<Divider />
-						{groupMembers}
-					</VStack>
-					<Box flex={1} justifyContent={"flex-end"}>
-						<DeleteGroupButton group={group} navigation={navigation} />
-						<Divider mb={2} />
-					</Box>
-				</VStack>
-			</ScrollView>
-		</Box>
-	);
+            <AddMembersButton navigation={navigation} group={group} />
+            <Divider />
+            {groupMembers}
+          </VStack>
+          <Box flex={1} justifyContent={"flex-end"}>
+            <DeleteGroupButton group={group} navigation={navigation} />
+            <Divider mb={2} />
+          </Box>
+        </VStack>
+      </ScrollView>
+    </Box>
+  );
 };
 
 export default observer(EditGroup);
--- Components/GroupDetail/index.js
+++ Components/GroupDetail/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 import {
-	HStack,
-	Spinner,
-	ScrollView,
-	VStack,
-	Input,
-	Icon,
-	Box,
-	Divider,
-	useDisclose,
+  HStack,
+  Spinner,
+  ScrollView,
+  VStack,
+  Input,
+  Icon,
+  Box,
+  Divider,
+  useDisclose,
+  useToast,
 } from "native-base";
 import { KeyboardAvoidingView, TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
@@ -24,100 +25,112 @@ import StaggerButton from "./StaggerButton";
 import groupStore from "../../stores/groupStore";
 import authStore from "../../stores/authStore";
 import { Platform } from "react-native";
+import { socket } from "../../stores/instance";
 
 const GroupDetail = ({ route, navigation }) => {
-	const [newMessage, setNewMessage] = useState("");
-	const { group } = route.params;
-	const { isOpen, onToggle, onClose } = useDisclose();
+  const [newMessage, setNewMessage] = useState("");
+  const { group } = route.params;
+  const { isOpen, onToggle, onClose } = useDisclose();
+  const toast = useToast();
 
-	if (groupStore.isLoading) {
-		return <Spinner />;
-	}
+  if (groupStore.isLoading) {
+    return <Spinner />;
+  }
 
-	const groupDetailContent = [...group.chat, ...group.polls];
+  const groupDetailContent = [...group.chat, ...group.polls];
 
-	//create a sort method using moment
+  //create a sort method using moment
 
-	const content = groupDetailContent.map((element) => {
-		if (element.contentType === "chat") {
-			return <ChatItem key={element._id} chatData={element} group={group} />;
-		} else if (element.contentType === "poll") {
-			return <PollItem key={element._id} pollData={element} group={group} />;
-		}
-	});
+  const content = groupDetailContent.map((element) => {
+    if (element.contentType === "chat") {
+      return <ChatItem key={element._id} chatData={element} group={group} />;
+    } else if (element.contentType === "poll") {
+      return <PollItem key={element._id} pollData={element} group={group} />;
+    }
+  });
 
-	const handleSubmit = () => {
-		const message = {
-			sentFrom: authStore.user._id,
-			message: newMessage,
-			members: group.members,
-			group: group._id,
-		};
-		groupStore.sendChatToGroup(group._id, message);
-		setNewMessage("");
-	};
+  socket.on("navigate-home", () => {
+    toast.show({
+      title: "Group Has been deleted!",
+      status: "info",
+      placement: "top",
+      duration: 1500,
+    });
+    navigation.navigate("Groups");
+  });
 
-	return (
-		<Box flex={1} bg="#dbd9dc">
-			<Divider />
+  const handleSubmit = () => {
+    const message = {
+      sentFrom: authStore.user._id,
+      message: newMessage,
+      members: group.members,
+      group: group._id,
+    };
+    groupStore.sendChatToGroup(group._id, message);
+    setNewMessage("");
+  };
 
-			<VStack mt="5" flex={1}>
-				<ScrollView>{content}</ScrollView>
-			</VStack>
+  return (
+    <Box flex={1} bg="#dbd9dc">
+      <Divider />
 
-			<Divider mb="2" />
+      <VStack mt="5" flex={1}>
+        <ScrollView>{content}</ScrollView>
+      </VStack>
 
-			<StaggerButton
-				navigation={navigation}
-				group={group}
-				onToggle={onToggle}
-				isOpen={isOpen}
-				onClose={onClose}
-			/>
+      <Divider mb="2" />
 
-			<KeyboardAvoidingView>
-				<VStack alignItems="center" mb="5">
-					<HStack alignItems="center">
-						<Input
-							returnKeyType="send"
-							returnKeyLabel="send"
-							onSubmitEditing={handleSubmit}
-							color="black"
-							placeholder="Message"
-							placeholderTextColor={Colors.darkBg}
-							variant="filled"
-							bg={Colors.lightBg}
-							borderRadius="50"
-							w="85%"
-							py={Platform.OS === "ios" ? "4" : "2"}
-							px="3"
-							mx="1"
-							_focus={{ borderColor: Colors.primary }}
-							value={newMessage}
-							onChangeText={(newMessage) => setNewMessage(newMessage)}
-							borderWidth="0"
-							InputLeftElement={
-								<TouchableOpacity>
-									<Icon
-										onPress={onToggle}
-										size="sm"
-										ml="3"
-										color={Colors.primary}
-										as={<AntDesign name="pluscircle" />}
-									/>
-								</TouchableOpacity>
-							}
-						/>
-						<MaterialCommunityIcons
-							name="send-circle-outline"
-							size={32}
-							color={Colors.primary}
-							onPress={handleSubmit}
-						/>
-					</HStack>
-				</VStack>
-			</KeyboardAvoidingView>
-		</Box>
-	);
+      <StaggerButton
+        navigation={navigation}
+        group={group}
+        onToggle={onToggle}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+
+      <KeyboardAvoidingView>
+        <VStack alignItems="center" mb="5">
+          <HStack alignItems="center">
+            <Input
+              returnKeyType="send"
+              returnKeyLabel="send"
+              onSubmitEditing={handleSubmit}
+              color="black"
+              placeholder="Message"
+              placeholderTextColor={Colors.darkBg}
+              variant="filled"
+              bg={Colors.lightBg}
+              borderRadius="50"
+              w="85%"
+              py={Platform.OS === "ios" ? "4" : "2"}
+              px="3"
+              mx="1"
+              _focus={{ borderColor: Colors.primary }}
+              value={newMessage}
+              onChangeText={(newMessage) => setNewMessage(newMessage)}
+              borderWidth="0"
+              InputLeftElement={
+                <TouchableOpacity>
+                  <Icon
+                    onPress={onToggle}
+                    size="sm"
+                    ml="3"
+                    color={Colors.primary}
+                    as={<AntDesign name="pluscircle" />}
+                  />
+                </TouchableOpacity>
+              }
+            />
+            <MaterialCommunityIcons
+              name="send-circle-outline"
+              size={32}
+              color={Colors.primary}
+              onPress={handleSubmit}
+            />
+          </HStack>
+        </VStack>
+      </KeyboardAvoidingView>
+    </Box>
+  );
 };
 export default observer(GroupDetail);
