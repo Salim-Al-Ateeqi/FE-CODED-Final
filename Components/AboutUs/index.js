c15b3959af85ad2de6afea4a5be16e171bbe545b
fahadalduaij <fahad_osama@hotmail.com>
2021-12-14 19:52:08 +0300

    edit feedback and add About us page

--- Components/AboutUs/index.js
+++ Components/AboutUs/index.js
@@ -1,11 +1,88 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+	Box,
+	Heading,
+	AspectRatio,
+	Image,
+	Text,
+	Center,
+	HStack,
+	Stack,
+	Divider,
+	Button,
+} from "native-base";
+import { useNavigation } from "@react-navigation/native";
+
+// components
+import { Colors } from "../../assets/Theme/Colors";
 
 const AboutUs = () => {
+	const navigation = useNavigation();
+
 	return (
-		<View>
-			<Text>About Us Not Finish</Text>
-		</View>
+		<Center flex={1}>
+			<Box
+				flex={1}
+				maxW="full"
+				overflow="hidden"
+				borderColor="coolGray.200"
+				borderWidth="0"
+				_dark={{
+					borderColor: "coolGray.600",
+					backgroundColor: Colors.lightBg,
+				}}
+				_light={{
+					backgroundColor: Colors.lightBg,
+				}}
+			>
+				<Center mt={5}>
+					<Image
+						source={require("../../assets/Media/PollLogo.png")}
+						alt="image"
+						size={"56"}
+					/>
+
+					<Divider mt={5} />
+				</Center>
+
+				<Stack p="4" space={3}>
+					<Stack space={2}>
+						<Heading size="md" ml="-1">
+							Poll App
+						</Heading>
+						<Text
+							fontWeight="semibold"
+							fontSize="xs"
+							_light={{
+								color: Colors.primary,
+							}}
+							_dark={{
+								color: Colors.primary,
+							}}
+							fontWeight="500"
+							ml="-0.5"
+							mt="-1"
+						>
+							Poll and watch a movie.
+						</Text>
+					</Stack>
+					<Text fontWeight="400">
+						Our application is about movie polls. So if you are confuse about
+						what to watch tonight? Make a group and votes with your family or
+						friends.
+					</Text>
+					<Button
+						w={"45%"}
+						mt={5}
+						alignSelf={"center"}
+						onPress={() => navigation.navigate("Feedback")}
+						style={{ backgroundColor: Colors.primary }}
+					>
+						Send Feedback
+					</Button>
+				</Stack>
+			</Box>
+		</Center>
 	);
 };
 
